9ea826174aa4915fafa73c3b6dcfc28cdee2751d
Aleksandr <57950063+Infqq@users.noreply.github.com>
2021-03-21 11:40:38 +0400

    lzt BETA

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="lolzapi",
+    version="0.0.1",
+    author="infqq",
+    description="Tool for lolz.guru",
+    url="https://github.com/Infqq/lolzapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
